7a2ba47bc1478f8ed832977759a6cb3114fb0356
syntherat <sounakpal06@gmail.com>
2025-12-13 22:44:39 +0530

    add new spline

---
+++ client/src/components/IntroSpline/IntroSpline.jsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline';
+import './IntroSpline.css';
+
+export default function IntroSpline() {
+  return (
+    <div className='intro-spline'>
+      <Spline scene="https://prod.spline.design/kqDqFB0gIyYSqHEw/scene.splinecode" /> 
+    </div>
+  );
+}
+  
