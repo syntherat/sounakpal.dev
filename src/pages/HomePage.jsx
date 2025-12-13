8287458f8e26f76e3bde979daa1e58dd42bf1ada
syntherat <sounakpal06@gmail.com>
2025-12-13 16:51:48 +0530

    update work details

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,40 +3,42 @@ import Hero from '../components/Hero/Hero';
 import SkillsShowcase from '../components/SkillsShowcase/SkillsShowcase';
 import RibbonSeparator from '../components/ui/RibbonSeparator.jsx/RibbonSeparator';
 import WorkSection from '../components/WorkSection/WorkSection';
+import ExperienceSection from '../components/Experience/ExperienceSection';
+import GetInTouch from '../components/GetInTouch/GetInTouch';
 
 const projects = [
   {
-    id: "starforge",
-    title: "AI SaaS Landing",
-    description: "A sleek AI SaaS landing page with a user-friendly design.",
-    year: "2024",
+    id: "insights",
+    title: "Insights Club Web",
+    description: "A sleek user-friendly college club journalism webpage.",
+    year: "2025",
     gradient: ["#ff2a9d", "#ff5aa5", "#b61cff"], // top -> bottom feel
     mockups: ["/work-images/insights-thumb2.png","/work-images/Insights-thumb1.png"], // stacked
-    tags: ["React", "GSAP", "UI"],
+    tags: ["React", "Motion.dev", "Tailwind"],
     href: "/projects/starforge",
     github: "https://github.com/you/ai-saas", 
     live: "https://ai-saas.vercel.app",        
   },
   {
-    id: "snips",
-    title: "Code Snippets Platform",
-    description: "Create and share code snippets with a clean, intuitive UI.",
-    year: "2024",
+    id: "rxpress",
+    title: "Rxpress Logistics Web",
+    description: "A simple logistics landing page with live shipment tracking.",
+    year: "2025",
     gradient: ["#15d6c5", "#12c9be", "#0aa2ff"],
     image: "/work-images/Rxpress-Thumb.png", // single
-    tags: ["Next.js", "Tailwind", "Prisma"],
+    tags: ["React", "Tailwind", "NeonDB"],
     href: "/projects/snips",
     github: "https://github.com/you/ai-saas", 
     live: "https://ai-saas.vercel.app", 
   },
   {
-    id: "snipss",
-    title: "Code Snippets Platform",
-    description: "Create and share code snippets with a clean, intuitive UI.",
-    year: "2024",
-    gradient: ["#15d6c5", "#12c9be", "#0aa2ff"],
+    id: "pahal",
+    title: "Pahal Foundation",
+    description: "A cute uplifting college charity foundation with custom donations.",
+    year: "2025",
+    gradient: ["#7e22ce", "#b675f5", "#842ad3ff"],
     image: "/work-images/Pahal-thumb.png", // single
-    tags: ["Next.js", "Tailwind", "Prisma"],
+    tags: ["React", "Tailwind"],
     href: "/projects/snips",
     github: "https://github.com/you/ai-saas", 
     live: "https://ai-saas.vercel.app", 
@@ -50,6 +52,8 @@ export default function HomePage({ onHeroMenuVisibilityChange }) {
       <RibbonSeparator />
       <SkillsShowcase />
       <WorkSection projects={projects} viewAllHref="/projects" />
+      <ExperienceSection />
+      <GetInTouch />
     </>
 
   );
